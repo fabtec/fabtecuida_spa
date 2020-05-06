e7552e2c9558108b23af1f668cbc852ea41d74ce
Raul Pino <pinox101@gmail.com>
2020-05-06 08:08:39 -0400

    example of using tabler added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Card, Grid } from "tabler-react";
+
+import "tabler-react/dist/Tabler.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <Grid.Row cards>
+        <Grid.Col>
+          <Card title="Title" body="A" />
+        </Grid.Col>
+        <Grid.Col>
+          <Card
+            title="Title"
+            body={`orem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+        deleniti fugit incidunt.`}
+          />
+        </Grid.Col>
+        <Grid.Col>
+          <Card title="Title" body="C" />
+        </Grid.Col>
+      </Grid.Row>
+      <Grid.Row cards deck>
+        <Grid.Col>
+          <Card title="Title" body="A" />
+        </Grid.Col>
+        <Grid.Col>
+          <Card
+            title="Title"
+            body={`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+          deleniti fugit incidunt.`}
+          />
+        </Grid.Col>
+        <Grid.Col>
+          <Card title="Title" body="C" />
+        </Grid.Col>
+      </Grid.Row>
+
       </header>
     </div>
   );
