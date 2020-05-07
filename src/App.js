45f9aa5c92f64f3728124dad7337a07d5ee76cf5
Raul Pino <pinox101@gmail.com>
2020-05-07 18:55:51 -0400

    example of login page added

--- src/App.js
+++ src/App.js
@@ -1,48 +1,19 @@
-import React from 'react';
-import './App.css';
-import { Card, Grid } from "tabler-react";
+import React from 'react'
+import './App.css'
+import { LoginPage } from 'tabler-react'
 
-import "tabler-react/dist/Tabler.css";
+import 'tabler-react/dist/Tabler.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
 
-      <Grid.Row cards>
-        <Grid.Col>
-          <Card title="Title" body="A" />
-        </Grid.Col>
-        <Grid.Col>
-          <Card
-            title="Title"
-            body={`orem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
-        deleniti fugit incidunt.`}
-          />
-        </Grid.Col>
-        <Grid.Col>
-          <Card title="Title" body="C" />
-        </Grid.Col>
-      </Grid.Row>
-      <Grid.Row cards deck>
-        <Grid.Col>
-          <Card title="Title" body="A" />
-        </Grid.Col>
-        <Grid.Col>
-          <Card
-            title="Title"
-            body={`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
-          deleniti fugit incidunt.`}
-          />
-        </Grid.Col>
-        <Grid.Col>
-          <Card title="Title" body="C" />
-        </Grid.Col>
-      </Grid.Row>
+      <LoginPage />
 
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
