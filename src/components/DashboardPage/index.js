ff0b8ac5d0e00f8fe0ad9f347fcddcc75d9ba1ee
Raul Pino <pinox101@gmail.com>
2020-05-08 00:42:52 -0400

    basic dashboard page added just for alerting login success

---
+++ src/components/DashboardPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Alert } from 'tabler-react'
+
+function DashboardPage () {
+  return (
+    <Alert type='success'>
+            En Dashboard!
+    </Alert>
+  )
+}
+
+export default DashboardPage
