8b33098ad38a5450da928469597173eda200e6cd
Raul Pino <pinox101@gmail.com>
2020-05-17 13:45:39 -0400

    load spinner in case login aven't finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import * as R from 'ramda';
+import * as RA from 'ramda-adjunct';
 import './App.css'
 import 'tabler-react/dist/Tabler.css'
 import {
@@ -7,11 +9,12 @@ import {
   Route
 } from 'react-router-dom'
 import Api from './services/api'
+import LoadingPage from './components/LoadingPage'
 import LoginPage from './components/LoginPage'
 import DashboardPage from './components/DashboardPage'
 
 function App () {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(null)
 
   Api.verifyUser()
     .then((isValid) => {
@@ -23,6 +26,12 @@ function App () {
     })
 
   const renderRoot = () => {
+    if (RA.isNilOrEmpty(isLoggedIn)) {
+      return (
+        <LoadingPage />
+      )
+    }
+
     if (!isLoggedIn) {
       return (
         <LoginPage />
