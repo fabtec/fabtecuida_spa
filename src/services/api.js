c3c00143c3e9edc5fbc681d6e6bbde27016f12bb
Raul Pino <pinox101@gmail.com>
2020-05-17 19:07:56 -0400

    order creation endpoint call added

--- src/services/api.js
+++ src/services/api.js
@@ -5,6 +5,29 @@ import Cookies from 'js-cookie'
 const API_HOST = 'http://localhost:8000'
 
 export default class Api {
+  static setTokens ({ access, refresh }) {
+    Cookies.set('jwt_access_token', access)
+    Cookies.set('jwt_refresh_token', refresh)
+  }
+
+  static getTokens () {
+    return {
+      jwtAccessToken: Cookies.get('jwt_access_token'),
+      jwtRefreshToken: Cookies.get('jwt_refresh_token')
+    }
+  }
+
+  static getAuthHeaders () {
+    const { jwtAccessToken } = Api.getTokens()
+    if (!jwtAccessToken) {
+      return {}
+    }
+
+    return {
+      Authorization: `Bearer ${jwtAccessToken}`
+    }
+  }
+
   static loginUser ({ username, password }) {
     return axios({
       method: 'post',
@@ -37,15 +60,26 @@ export default class Api {
       })
   }
 
-  static setTokens ({ access, refresh }) {
-    Cookies.set('jwt_access_token', access)
-    Cookies.set('jwt_refresh_token', refresh)
+  static getEntities () {
+    return axios({
+      headers: Api.getAuthHeaders(),
+      method: 'get',
+      url: `${API_HOST}/api/entities/`
+    })
+      .then((res) => {
+        return res.data
+      })
   }
 
-  static getTokens () {
-    return {
-      jwtAccessToken: Cookies.get('jwt_access_token'),
-      jwtRefreshToken: Cookies.get('jwt_refresh_token')
-    }
+  static createOrder (data) {
+    return axios({
+      headers: Api.getAuthHeaders(),
+      method: 'post',
+      url: `${API_HOST}/api/orders/`,
+      data
+    })
+      .then((res) => {
+        return res.data
+      })
   }
 }
