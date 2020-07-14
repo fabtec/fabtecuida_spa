ca56d65c32e2c42097ffc1570321814145cf9e64
Fabián Vargas <fvargassanchez34@gmail.com>
2020-07-14 17:20:00 -0400

    config spanish lenguage & show events

--- src/components/DashboardPage/index.js
+++ src/components/DashboardPage/index.js
@@ -2,8 +2,11 @@ import React from 'react'
 import { Alert } from 'tabler-react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import esLocale from '@fullcalendar/core/locales/es';/**/
 
-function DashboardPage () {
+
+
+function DashboardPage (events) {
   return (
     <>
       <Alert type='success'>
@@ -13,9 +16,23 @@ function DashboardPage () {
       <FullCalendar
         plugins={[ dayGridPlugin ]}
         initialView="dayGridMonth"
+        locale="esLocale"
+        firstDay="1"
+        buttonText={{
+            "today":    'Hoy',
+            "month":    'Mes',
+            "week":     'Semana',
+            "day":      'Día',
+            "list":     'Lista'
+            }}
+        events={[
+          { title: 'event 1', date: '2020-07-06' },
+          { title: 'event 2', date: '2020-07-20' }
+        ]}
       />
     </>
   )
 }
+
 
 export default DashboardPage
