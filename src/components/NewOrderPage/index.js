65876b08976ca9226c1c763b9e834d9615bd07db
Fabián Vargas <fvargas@oxidocs.com>
2020-07-15 01:19:46 -0400

    add design to form new orders

--- src/components/NewOrderPage/index.js
+++ src/components/NewOrderPage/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import * as R from 'ramda'
-import { Row, Form, Button } from 'react-bootstrap';
+import { Row, Form, Button, Card } from 'react-bootstrap';
 import Api from '../../services/api'
 
 function NewOrderPage () {
@@ -29,47 +29,25 @@ function NewOrderPage () {
   }
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <fieldset>
-        <Form.Group as={Row}>
-          <Form.Label as="legend" column sm={2}>
-            Nueva Solicitud
-          </Form.Label>
+    <div class="row justify-content-center">
+       <Card body className="col-12 col-md-6 shadow">
 
-          <EntitiesSelector
-            list={entitiesList}
-            value={selectedEntity}
-            onChange={onSelectedEntityChange}
-          />
-        
-          <Button block color='primary' type='submit'>
-            Submit
+        <Form>
+          <Form.Group controlId="entity">
+            <Form.Label>Tu Entidad</Form.Label>
+            <EntitiesSelector
+                  list={entitiesList}
+                  value={selectedEntity}
+                  onChange={onSelectedEntityChange}
+                />
+          </Form.Group>
+
+          <Button className="shadow" block variant="primary" type="submit">
+          Enviar
           </Button>
-        </Form.Group>
-      </fieldset>
-    </Form>
-    // <Grid.Row cards alignItems='center'>
-    //   <Grid.Col>
-    //     <Card>
-    //       <Card.Header>
-    //         <Card.Title>Nueva Solicitud</Card.Title>
-    //       </Card.Header>
-    //       <Card.Body>
-
-    //         <Form onSubmit={handleSubmit}>
-    //           <Form.Group>
-    //             <EntitiesSelector list={entitiesList} value={selectedEntity} onChange={onSelectedEntityChange} />
-
-    //             <Button block color='primary' type='submit'>
-    //               Submit
-    //             </Button>
-    //           </Form.Group>
-    //         </Form>
-
-    //       </Card.Body>
-    //     </Card>
-    //   </Grid.Col>
-    // </Grid.Row>
+        </Form>
+      </Card>
+    </div>
   )
 }
 
@@ -87,7 +65,7 @@ function EntitiesSelector ({ list, value, onChange }) {
   })(list))
 
   return (
-    <Form.Control as="select" label='Tu Entidad' value={value} onChange={onChange}>
+    <Form.Control as="select" value={value} onChange={onChange}>
       {entities}
     </Form.Control>
   )
